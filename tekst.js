3434931af0fbdd9a0728001f609943f83ab46e62
pepi05 <72278293+pepi05@users.noreply.github.com>
2020-10-06 16:49:44 +0200

    izmenet tekst vo text.js

--- tekst.js
+++ tekst.js
@@ -1,2 +1,3 @@
 console.log ("Contrary to popular belief, Lorem Ipsum is not simply random text.");
 console.log ("It has roots in a piece of classical Latin literature from 45 BC");
+console.log ("It is a long established fact that a reader will be distracted by the readable content of a page");
