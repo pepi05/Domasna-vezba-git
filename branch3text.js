4fe493826f53e081b9e38287b4e12b4fbaf2e038
pepi05 <72278293+pepi05@users.noreply.github.com>
2020-10-07 20:31:34 +0200

    uste edna dodadena linija posle pulot od masterot

--- branch3text.js
+++ branch3text.js
@@ -2,3 +2,4 @@ console.log("Opst tekst");
 console.log("tekst pishan vo branch 3");
 console.log("napravivme pull od master");
 console.log("Najnova promena");
+console.log("uste edna promena posle napraven pull od masterot");
